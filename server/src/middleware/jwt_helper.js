685a39eb19ad9281d7075c461e4f6c8e133816eb
Elena Zht <elenazht@gmail.com>
2025-05-18 12:19:12 +0300

    Create jwt and permission guard for protected routes

--- server/src/middleware/jwt_helper.js
+++ server/src/middleware/jwt_helper.js
@@ -32,37 +32,38 @@ export const validateToken = (token, secret) => {
     }
 };
 
-// export const verifyAccessToken = (req, res, next) => {
-//     const authHeader = req.headers['authorization'];
-//     const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN format
+export const verifyAccessToken = (req, res, next) => {
+  console.log("jwt verifyAccessToken req.", req)
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN format
     
-//     if (!token) {
-//       return res.status(401).json({ message: 'Access denied. No token provided.' });
-//     }
+    if (!token) {
+      return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
     
-//     const decoded = validateToken(token, ACCESS_TOKEN_SECRET);
+    const decoded = validateToken(token, ACCESS_TOKEN_SECRET);
     
-//     if (!decoded) {
-//       return res.status(403).json({ message: 'Invalid or expired token' });
-//     }
+    if (!decoded) {
+      return res.status(403).json({ message: 'Invalid or expired token' });
+    }
     
-//     req.user = decoded;
-//     next();
-// };
+    req.user = decoded;
+    next();
+};
 
-// export const verifyRefreshToken = (req, res, next) => {
-//     const token = req.body.refreshToken || req.cookies.refreshToken;
+export const verifyRefreshToken = (req, res, next) => {
+    const token = req.body.refreshToken || req.cookies.refreshToken;
     
-//     if (!token) {
-//       return res.status(401).json({ message: 'Refresh token not provided' });
-//     }
+    if (!token) {
+      return res.status(401).json({ message: 'Refresh token not provided' });
+    }
     
-//     const decoded = validateToken(token, REFRESH_TOKEN_SECRET);
+    const decoded = validateToken(token, REFRESH_TOKEN_SECRET);
     
-//     if (!decoded) {
-//       return res.status(403).json({ message: 'Invalid or expired refresh token' });
-//     }
+    if (!decoded) {
+      return res.status(403).json({ message: 'Invalid or expired refresh token' });
+    }
     
-//     req.user = decoded;
-//     next();
-// };
+    req.user = decoded;
+    next();
+};
--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { signUpUser, deleteAccount } from '../controllers/usersController.js';
+import { authenticate } from '../middleware/auth.js';
+import { isAccountOwner } from '../middleware/permissions.js';
+
 
 const router = express.Router();
 
 router.post('/signup', signUpUser);
-router.delete('/:user_id', deleteAccount);
+router.delete('/:user_id', authenticate, isAccountOwner, deleteAccount);
 
 export default router;
